10cf4793ffbee335ccec1d961501f995f45eb64c
qghs <qghs@example.com>
2025-03-21 12:01:52 +0800

    fix: update -v -init

--- src/cli.ts
+++ src/cli.ts
@@ -4,20 +4,50 @@ import { Command } from 'commander';
 import { loadConfig } from './config';
 import { startServer } from './server';
 import { CommandOptions } from './types';
+import { writeFileSync } from 'fs';
+import { join } from 'path';
 
 const program = new Command();
+const packageJson = require('../package.json');
 
 program
-    .version('1.0.0')
+    .version(packageJson.version, '-v, --version')
     .option('-c, --config <path>', '配置文件路径')
     .option('--cf <path>', '配置文件路径')
-    .option('--config-file <path>', '配置文件路径')
-    .parse(process.argv);
+    .option('--config-file <path>', '配置文件路径');
 
-const options = program.opts() as CommandOptions;
-const config = loadConfig(options);
+program
+    .command('init')
+    .description('初始化配置文件')
+    .action(() => {
+        const defaultConfig = {
+            port: 8080,
+            dir: './',
+            proxy: {
+                '/api': {
+                    target: 'http://localhost:3000',
+                    changeOrigin: true,
+                    pathRewrite: {
+                        '^/api': ''
+                    }
+                }
+            }
+        };
 
-startServer(config).catch((error) => {
-    console.error('Server failed to start:', error);
-    process.exit(1);
-});
+        const configPath = join(process.cwd(), 'agent_config.json');
+        writeFileSync(configPath, JSON.stringify(defaultConfig, null, 2));
+        console.log('配置文件已创建：agent_config.json');
+        process.exit(0);
+    });
+
+program.parse(process.argv);
+
+if (!program.args.length) {
+    const options = program.opts() as CommandOptions;
+    const config = loadConfig(options);
+
+    startServer(config).catch((error) => {
+        console.error('Server failed to start:', error);
+        process.exit(1);
+    });
+}
